229f86852c5415e69c175bf8c03246580f3f5f1a
Juti Buican <jbuican19@gmail.com>
2021-10-11 22:49:53 +0100

    Auth demo

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,45 @@
+import connectToDatabase from "../../../lib/db";
+import {hashPassword} from "./auth";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const {email, password} = data;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          "Invaid input - password should also be at least 7 characters long.",
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const existingUser = await db.collection("users").findOne({email: email});
+
+    if (existingUser) {
+      res.status(422).json({message: "User exists already"});
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({message: "Created user!"});
+    client.close();
+  }
+}
+
+export default handler;
---
+++ pages/auth.js
@@ -0,0 +1,6 @@
+import AuthForm from "../components/auth/auth-form";
+function AuthPage() {
+  return <AuthForm />;
+}
+
+export default AuthPage;
